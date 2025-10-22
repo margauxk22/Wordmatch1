bd5caa3527df6410134e7f8eb490a76e2875418d
margauxk22 <margauxk2@nycstudents.net>
2025-10-22 11:16:16 -0400

    Finished project

--- src/WordMatch.java
+++ src/WordMatch.java
@@ -1,11 +1,11 @@
 public class WordMatch {
 
-    /** The secret string. */
+
     private String secret;
 
 
 
-    /** Constructs a WordMatch object with the given secret string of lowercase letters. */
+
     public WordMatch(String word) {
         secret = word;
     }
@@ -14,9 +14,7 @@ public class WordMatch {
         return secret;
     }
 
-    /** Returns a score for guess, as described in part (a).
-     * Precondition: 0 < guess.length() <= secret.length()
-     */
+
     public int scoreGuess(String guess) {
         int score = 0;
         for(int i = 0; i <= secret.length() - guess.length(); i++)
@@ -25,11 +23,7 @@ public class WordMatch {
         return score * guess.length() * guess.length();
     }
 
-    /** Returns the better of two guesses, as determined by scoreGuess and the rules for a
-     * tie-breaker that are described in part (b).
-     * Precondition: guess1 and guess2 contain all lowercase letters.
-     * guess1 is not the same as guess2.
-     */
+
     public String findBetterGuess(String guess1, String guess2) {
         int score1 = scoreGuess(guess1);
         int score2 = scoreGuess(guess2);
